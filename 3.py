e41b67a79be34e419f7e0af4f48c22d83d77c31c
cotyq <cotii.q@gmail.com>
2024-12-26 00:14:21 -0300

    Remove duplicated files

--- 3.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open("input3") as file:
-    linea = "".join(line.strip() for line in file)
-    patron = r"don't\(\).*?do\(\)"
-    coincidencias = re.findall(patron, linea)
-    print(len(coincidencias))
